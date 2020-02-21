0d5278f633bbc2c8c31b8893dcb19f2e626770aa
Anderson Jimenez <ajimenez@atrivia.do>
2020-02-21 10:14:48 -0400

    [UPD]  - add wrap method, this method sum all the fitted columns 	   and between them add a tab spacer.
      On branch screen_text_wrapper
     Changes to be committed:
    	modified:   utils.py
    	modified:   wrapper.py

--- utils.py
+++ utils.py
@@ -1,6 +1,27 @@
-def text_sum(list_of_str,spacer=''):
+def text_sum(list_of_str:list,spacer=' '):
     res = ''
     for txt in list_of_str:
-        if isinstance(str,txt):
+        if not isinstance(txt,str):
             raise TypeError('All elements must be instance of str')
-        res += txt + spacer
+        res += txt + (list_of_str.index(txt) == len(list_of_str)-1 and '' or spacer)
+    return res
+
+def last_word(list_of_str, size_limit):
+    index = 0
+    for s in range(0,len(list_of_str)):
+        if len(text_sum(list_of_str[:s])) > size_limit:
+            break
+        index = s
+    return index
+
+if __name__ == '__main__':
+    txt = text_sum(['a','b','c'])
+    txt2 = ['as','fast','i', 'can']
+    print(txt)
+    assert(txt != 'a b c') , "Not equal"
+    assert(len(txt) != len('a b c')) , "size don't correspond"
+    test = last_word(txt2, 3)
+    print(test)
+    print(text_sum(txt2[:test]))
+    assert(test == 1 )
+    
--- wrapper.py
+++ wrapper.py
@@ -1,4 +1,4 @@
-from utils import text_sum
+from utils import text_sum, last_word
 
 class Wrapper():
 
@@ -7,52 +7,68 @@ class Wrapper():
         The brown fox jumped into the darkest hole of depression. ||| Lorem ipsum mandatorem t ominuos nebula.
         All his friends told him to go to a psych consult but he didnt wanted. ||| ryphaza is the most funny hidden code of gta san andreas.
     """
-    separator = '|||'
+    separator = ' ||| '
     special_char = {
         'break': '\n',
-        'tab': '\t' 
+        'tab': '\t'
     }
 
-    def __init__(self, text, col_width=0):
+    def __init__(self, text, col_width, separation_size):
         self.col_width = col_width
         self.raw_text = text
         self.text_table = []
+        self.sep_size = separation_size
   
     def _text2matrix(self):
-        lines = self.raw_text.splitlines(keepends=True)
+        lines = self.raw_text.splitlines()
         res = []
         for l in lines:
-            cols = l.split(self.separator)
+            cols = [words.split(' ') for words in l.split(self.separator)]
             res.append(cols)
         self.text_table = res
             
     def _fit_width(self):
+        self._text2matrix()
+        lines_lenght = len(self.text_table)
         l = 0
-        lines = len(self.text_table)
-        while l < lines:
-            c = 0
+        while l < lines_lenght:
             for c in range(0,len(self.text_table[l])):
-                if len(self.text_table[l][c]) <= self.col_width:
+                txt = text_sum(self.text_table[l][c])
+                if not len(txt) > self.col_width:
+                    self.text_table[l][c] = txt
                     continue
-                words = self.text_table[l][c].split(' ')
-                index = 0
-                for w in words:
-                    pass
-                for_next_line = self.text_table[l][c][self.col_width:]
-                self.text_table[l][c] = self.text_table[l][c][:self.col_width]
-                if lines <= l+1:                    
-                        self.text_table.append(['']*len(self.text_table[l]))
-                        lines += 1
-                self.text_table[l+1][c] = "%s %s" % (for_next_line, self.text_table[l+1][c])
-            l += 1
-                
-                
-                
+                index = last_word(self.text_table[l][c], self.col_width)
+                next_line = self.text_table[l][c][index:]
+                self.text_table[l][c] = text_sum(self.text_table[l][c][:index])
+                if l+1 >= lines_lenght:
+                    self.text_table.append([[]] * len(self.text_table[l]))
+                    lines_lenght += 1
+                self.text_table[l+1][c] = next_line + self.text_table[l+1][c] 
+            l += 1 
+
+    def wrap(self):
+        self._fit_width()
+        res= ""
+        for l in self.text_table:
+            line = ""
+            for c in l:
+                offset = self.col_width - len(c)
+                _fit = ""
+                if offset != 0:
+                    _fit += ' ' * offset
+                if not l.index(c) >= len(l) -1:
+                    _fit += '\t'.expandtabs(self.sep_size)
+                line += c + _fit 
+            res += line + '\n'
+        return res
+
+    
 if __name__ == "__main__":
     w = Wrapper("The brown fox jumped into the darkest hole of depression. ||| Lorem ipsum mandatorem t ominuos nebula.\n"\
          "All his friends told him to go to a psych consult but he didnt wanted. ||| ryphaza is the most funny hidden code of gta san andreas.",
-         30
-    )
-    w._text2matrix()
-    w._fit_width()
-    print(w.text_table)
+         30, 90
+    ) 
+    print(w.wrap())
+    print('\n')
+    #print(w.text_table)
+    
